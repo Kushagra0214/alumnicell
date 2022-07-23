af1351814bfa1e933671fab7c7589bb23f913528
Kushagra Srivastava <kushagrasrivastava908@gmail.com>
2022-07-23 19:35:59 +0530

    Login page added

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -3,6 +3,17 @@ import "./home.css";
 
 export default function Home(){
     return (
-        <h1>Home</h1>
+        <div className="home">
+            <div className="mask">
+                
+            </div>
+            <form className="login-form">
+                <label for='email'><strong>EMAIL ID </strong></label>
+                <input type="email" name="email"/><br />
+                <label for='password'><strong>PASSWORD</strong></label>
+                <input type="password" name="password"/><br/>
+                <button className="login-button">SIGN IN</button>
+            </form>
+        </div>
     )
 }
