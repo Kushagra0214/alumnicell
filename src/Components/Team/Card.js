b6755660fd14c7e55f34d95c8608d506e7157e78
Pranav Todkar <75238140+pranavtodkar@users.noreply.github.com>
2022-07-28 15:24:29 +0530

    Carousel responsive and data filled

--- src/Components/Team/Card.js
+++ src/Components/Team/Card.js
@@ -2,10 +2,10 @@ import React from 'react';
 import "./team.css";
 
 const Card = ({ id, members, colr }) => {
-    
+  // style={{ backgroundImage: "url("+members[id].imgsrc+")"}}
   return (
           <div className='card-box' style={{ backgroundColor:colr}}>
-            <div className='rounded-circle' style={{ backgroundImage: "url("+members[id].imgsrc+")"}}></div>
+            <div className='rounded-circle' ><img className='photo' src={members[id].imgsrc}></img></div>
             <div className='nam'>{members[id].name}</div>
             <div className='desc-label'>DESCRIPTION</div>
             
--- src/Components/Team/team.js
+++ src/Components/Team/team.js
@@ -6,10 +6,11 @@ export default function Team(){
     
     
     const [members, setMembers] = useState([
+        // Core team        
         {
-            name: 'Dr. Satyaprakash Ahirwar',
-            description: ["Post: Alumni Affairs Faculty Advisor", <br />,  "alumnicell@iitgoa.ac.in", <br />,  "+91 80978 42443"],
-            imgsrc: 'img/team/Prof.Satyaprakash.jpg',
+            name: 'Prashant Choudhary',
+            description: ["Post: Alumni Affairs Secretary", <br />,  "prashant.choudhary.20063@iitgoa.ac.in", <br />,  "+91 70652 88851"],
+            imgsrc: 'img/team/Prashant.jpeg',
         },
         {   
             name: 'Mr. Rishikesh Narayan',
@@ -17,9 +18,9 @@ export default function Team(){
             imgsrc: 'img/team/Prof.Rishikesh.jpg',
         },
         {
-            name: 'Prashant Choudhary',
-            description: ["Post: Alumni Affairs Secretary", <br />,  "prashant.choudhary.20063@iitgoa.ac.in", <br />,  "+91 70652 88851"],
-            imgsrc: 'img/team/Prashant.jpeg',
+            name: 'Dr. Satyaprakash Ahirwar',
+            description: ["Post: Alumni Affairs Faculty Advisor", <br />,  "alumnicell@iitgoa.ac.in", <br />,  "+91 80978 42443"],
+            imgsrc: 'img/team/Prof.Satyaprakash.jpg',
         },
         {
             name: 'Naga Tanuja Bathena',
@@ -27,11 +28,105 @@ export default function Team(){
             imgsrc: 'img/team/Tanuja.jpg',
         },
         {
-            
             name: 'Sanskar Pratap',
             description: ["Post: Alumni Cell Member", <br />,  "sanskar.20033@iitgoa.ac.in", <br />,  "+91 86918 16255"],
             imgsrc: 'img/team/Sanskar.jpeg',
         },
+        // Content team
+        {
+            name: 'Sanskar Pratap',
+            description: ["Post: Alumni Cell Member", <br />,  "sanskar.20033@iitgoa.ac.in", <br />,  "+91 86918 16255"],
+            imgsrc: 'img/team/Sanskar.jpeg',
+        },
+        {
+            name: 'Sanskar Pratap',
+            description: ["Post: Alumni Cell Member", <br />,  "sanskar.20033@iitgoa.ac.in", <br />,  "+91 86918 16255"],
+            imgsrc: 'img/team/Sanskar.jpeg',
+        },
+        { 
+            name: 'Sanskar Pratap',
+            description: ["Post: Alumni Cell Member", <br />,  "sanskar.20033@iitgoa.ac.in", <br />,  "+91 86918 16255"],
+            imgsrc: 'img/team/Sanskar.jpeg',
+        },
+        // Design Team
+        { 
+            name: 'Sanskar Pratap',
+            description: ["Post: Alumni Cell Member", <br />,  "sanskar.20033@iitgoa.ac.in", <br />,  "+91 86918 16255"],
+            imgsrc: 'img/team/Sanskar.jpeg',
+        },
+        { 
+            name: 'Sanskar Pratap',
+            description: ["Post: Alumni Cell Member", <br />,  "sanskar.20033@iitgoa.ac.in", <br />,  "+91 86918 16255"],
+            imgsrc: 'img/team/Sanskar.jpeg',
+        },
+        { 
+            name: 'Sanskar Pratap',
+            description: ["Post: Alumni Cell Member", <br />,  "sanskar.20033@iitgoa.ac.in", <br />,  "+91 86918 16255"],
+            imgsrc: 'img/team/Sanskar.jpeg',
+        },
+        { 
+            name: 'Sanskar Pratap',
+            description: ["Post: Alumni Cell Member", <br />,  "sanskar.20033@iitgoa.ac.in", <br />,  "+91 86918 16255"],
+            imgsrc: 'img/team/Sanskar.jpeg',
+        },
+        // Social Media Team
+        { 
+            name: 'Sanskar Pratap',
+            description: ["Post: Alumni Cell Member", <br />,  "sanskar.20033@iitgoa.ac.in", <br />,  "+91 86918 16255"],
+            imgsrc: 'img/team/Sanskar.jpeg',
+        },
+        { 
+            name: 'Sanskar Pratap',
+            description: ["Post: Alumni Cell Member", <br />,  "sanskar.20033@iitgoa.ac.in", <br />,  "+91 86918 16255"],
+            imgsrc: 'img/team/Sanskar.jpeg',
+        },
+        // DBMS Team
+        { 
+            name: 'Akash Khandelwal',
+            description: ["Post: DBMS Team Member", <br />,  "akash.khandelwal.21031@iitgoa.ac.in", <br />,  "+91 99230 41275"],
+            imgsrc: 'img/team/Sanskar.jpeg',
+        },
+        { 
+            name: 'Atul Mehta',
+            description: ["Post: DBMS Team Member", <br />,  "atul.mehta.21063@iitgoa.ac.in", <br />,  " +91 78779 94958"],
+            imgsrc: 'img/team/Sanskar.jpeg',
+        },
+        { 
+            name: 'Jay Jawale',
+            description: ["Post: DBMS Team Member", <br />,  "jay.jawale.21063@iitgoa.ac.in", <br />,  "+91 95796 08314"],
+            imgsrc: 'img/team/Sanskar.jpeg',
+        },
+        { 
+            name: 'Karan Sutariya',
+            description: ["Post: DBMS Team Member", <br />,  " karan.sutariya.21033@iitgoa.ac.in", <br />,  " +91 93283 80569"],
+            imgsrc: 'img/team/Sanskar.jpeg',
+        },
+        { 
+            name: 'Sujal Jani',
+            description: ["Post: DBMS Team Lead", <br />,  "jani.sujal.21042@iitgoa.ac.in", <br />,  "+91 82009 81070"],
+            imgsrc: 'img/team/Sanskar.jpeg',
+        },
+        { 
+            name: 'Umar Sayed',
+            description: ["Post: DBMS Team Member", <br />,  "sayed.umar.21031@iitgoa.ac.in", <br />,  "+91 90825 86538"],
+            imgsrc: 'img/team/Sanskar.jpeg',
+        },
+        { 
+            name: 'Utsav Bansal',
+            description: ["Post: DBMS Team Member", <br />,  "utsav.bansal.21042@iitgoa.ac.in", <br />,  "+91 62671 84086"],
+            imgsrc: 'img/team/Sanskar.jpeg',
+        },
+        { 
+            name: ' Yash Bhansali',
+            description: ["Post: DBMS Team Member", <br />,  "yash.bhansali.21042@iitgoa.ac.in", <br />,  "+91 73048 93913"],
+            imgsrc: 'img/team/Sanskar.jpeg',
+        },
+
+
+        
+
+
+        // Core team
         {
             name: 'Aumkar Lorekar',
             description: ["Post: Alumni Cell Member", <br />,  "aumkar.lorekar.20031@iitgoa.ac.in", <br />,  "+91 89281 02188"],
